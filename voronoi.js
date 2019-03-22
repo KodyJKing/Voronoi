d9d9635fd7713ee06d08d13b9d8d185d19b999c8
KodyJKing <kody.j.king@gmail.com>
2019-03-21 22:05:24 -0700

    Implemented center of mass for proper Lloyd Relaxation.

--- voronoi.js
+++ voronoi.js
@@ -111,8 +111,65 @@ class Polygon {
         }
         let average = points.reduce((a, b) => a.add(b), new Vector(0, 0)).multiply(1 / points.length)
         points.sort((a, b) => a.subtract(average).angle - b.subtract(average).angle)
-        return points
+        let halfPoints = [] // If the shape is closed, every point occured twice.
+        for (let i = 0; i < points.length; i += 2)
+            halfPoints.push(points[i])
+        return halfPoints
     }
+}
+
+function area(vertices) {
+    let area = 0
+    for (let i = 0; i < vertices.length; i++) {
+        let p0 = vertices[i]
+        let p1 = vertices[(i + 1) % vertices.length]
+        area += (p1.y - p0.y) * (p1.x + p0.x) / 2
+    }
+    return area
+}
+
+// Calculates center of mass by decomposing polygon into trapezoids.
+// This is like the shoelace method but for center of mass: https://jrkoenig.com/2016/10/20/a-geometric-derivation-of-the-shoelace-theorem/
+// The center of mass is computed as (∫∫ y dA) / area and (∫∫ x dA) / area.
+// Each line segment forms a trapezoid with the y axis.
+// For each trapezoid, the area and integral of y over the area are computed.
+// When y is decreasing, area and the y integral are negated,
+// effectively removing that trapezoid rather than adding it.
+// Similar logic is used for x.
+function centerOfMass(vertices) {
+    // cy * area = ∫∫ y dA = ∫ y x(y) dy = ∫ y (x0 + m (y - y0)) dy
+    // = ∫ x0 y - m y0 y + y^2 dy = ∫ (x0 - m y0) y + y^2 dy
+    // = 1/2 y^2 (x0 - m y0) + 1/3 m y^3
+    let integral = (y, m, x0, y0) => (m * (y ** 3) / 3) + ((x0 - m * y0) * (y ** 2) / 2)
+
+    let area = 0
+    let cxa = 0 // Center of mass' x times area
+    let cya = 0 // Center of mass' y times area
+    for (let i = 0; i < vertices.length; i++) {
+        let p0 = vertices[i]
+        let p1 = vertices[(i + 1) % vertices.length]
+        let y0 = p0.y
+        let x0 = p0.x
+        let y1 = p1.y
+        let x1 = p1.x
+        let dy = y1 - y0
+        let dx = x1 - x0
+
+        // Add signed area of trapezoid formed between line segment and the y-axis
+        area += dy * (x1 + x0) / 2
+
+        if (Math.abs(dy) > 0.001) {
+            cya += integral(y1, dx / dy, x0, y0)
+            cya -= integral(y0, dx / dy, x0, y0)
+        }
+
+        if (Math.abs(dx) > 0.001) {
+            // For x, use the reciprocal of the slope and reverse the direction.
+            cxa += integral(x0, dy / dx, y1, x1)
+            cxa -= integral(x1, dy / dx, y1, x1)
+        }
+    }
+    return new Vector(cxa / area, cya / area)
 }
 class VoronoiDiagram {
 
